167b9ff8dabb8174fe519a86dfde771972806c11
pratikdhole <pratik.d2011@gmail.com>
2023-01-15 14:23:40 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { useState } from 'react';
+import './style.css';
+
+export default function App() {
+  const [inputarr, setinputarr] = useState([]);
+
+  const [inputdata, setInputdata] = useState({
+    taskname: '',
+    duration: '',
+  });
+
+  function changehandle(e) {
+    setInputdata({ ...inputdata, [e.target.name]: e.target.value });
+  }
+  let { taskname, duration } = inputdata;
+
+  function changhandle() {
+    setinputarr([...inputarr, { taskname, duration }]);
+    setInputdata({ taskname: '', duration: '' });
+    console.log(inputarr);
+    console.log(inputdata);
+  }
+
+  function changehandle2(){
+    console.log();
+  }
+  return (
+    <div className="task2">
+      <form>
+        <div>
+          <label>task Name</label>
+          <input
+            type="text"
+            value={inputdata.taskname}
+            className="text1"
+            onChange={changehandle}
+            name="taskname"
+          />
+        </div>
+        <div>
+          <label>Duration</label>
+          <input
+            type="text"
+            value={inputdata.duration}
+            className="text1"
+            onChange={changehandle}
+            name="duration"
+          />
+        </div>
+        <button className="addlist" onClick={changhandle}>
+          Add list
+        </button>
+      </form>
+      <table >
+        <thead>
+          <tr>
+            <th>Task Name</th>
+            <th>Task Duration</th>
+          </tr>
+        </thead>
+        <tbody>
+                {                 
+                 inputarr.map{
+                  (info,ind)=> {
+                    return (
+                      <tr key={ind}>
+                      <td>{info.taskname}</td>
+                      <td>{info.duration}</td>
+                    </tr>
+                    )
+                  }
+                
+               }
+
+          </tbody>
+      </table>
+    </div>
+  );
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-add-data-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-teuzw8)
